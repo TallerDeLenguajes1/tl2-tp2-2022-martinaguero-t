90460207a21244783c31b142f2716956d9f72751
Martin Agüero Trevisan <aguerotrevisanmartin@gmail.com>
2022-09-09 18:14:14 -0300

    LogTest

---
+++ LogTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using NLog;
+
+class Program {
+
+    static int Main(string[] args){
+
+        Logger logger = LogManager.GetCurrentClassLogger();
+        
+        logger.Info("Prueba log info");
+        logger.Debug("Prueba log debug");
+
+        return 0;
+    }
+
+}
